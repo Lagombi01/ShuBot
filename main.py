5739d57c288b5d0de92ee8ad5ae8aa6a0fea38d9
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2022-08-05 18:27:40 +0100

    Tried some stuff :3

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import os
 import discord
-
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
 
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected!')
+
+
+@bot.event
+async def on_ready1():
+    print("Bot is ready!")
+
+
+@bot.command()
+async def test(ctx):
+    print("hi")
+
+
+@bot.command()
+async def gai(ctx):
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio('Gai.mp3')
+    print("a")
+    if not ctx.message.author.voice:
+        print("b")
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
 
 
 client.run(TOKEN)
