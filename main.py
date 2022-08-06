978f078ab39886f0d2e6d47deb257e9a5a53a336
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2022-08-06 04:59:18 +0100

    Ehhh + message responses

--- main.py
+++ main.py
@@ -49,4 +49,40 @@ async def cri(ctx):
     await asyncio.sleep(9)
     await vc.disconnect()
 
+
+@bot.command()
+async def ehh(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+
+    vc = await channel.connect()
+    audio = discord.FFmpegPCMAudio('Ehh.mp3')
+    player = vc.play(audio)
+    await asyncio.sleep(4)
+    await vc.disconnect()
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if bot.user.id != message.author.id:
+        if 'shu' in message.content.lower():
+            await message.reply("ShuUUuUuuuuUUuUUuu?!!?!?!")
+        elif 'gai' in message.content.lower():
+            await message.reply("GaiiiiIIIIIiiiIIIIiiIIii!?!?!?!?!?!")
+        elif 'inori' in message.content.lower():
+            await message.reply("InoriIIIIiiiIIIiIiiIiiIiiiI!!!!!!!!!")
+        elif 'captain' in message.content.lower():
+            await message.reply("arf arf")
+        elif 'incest' in message.content.lower() or 'christmas' in message.content.lower():
+            await message.reply("MANAAAAAAAAAAAAAAA!")
+        elif 'woof' in message.content.lower():
+            await message.reply("woof woof")
+        elif 'sugar' in message.content.lower():
+            await message.reply("Sugar???")
+
+
 bot.run(TOKEN)
