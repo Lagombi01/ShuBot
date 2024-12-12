9e4888f4aba19143aa6876184145539420da6e6e
Anivarth <Lagombi01@gmail.com>
2024-12-12 12:48:56 +0000

    Added Error Handling

--- cogs/Modules/button.py
+++ cogs/Modules/button.py
@@ -12,7 +12,7 @@ class Button(discord.ui.Button):
             await self.clip(interaction)
         except discord.errors.ClientException:
             await interaction.response.send_message("Too many requests, slow down!", ephemeral=True)
-        except e:
+        except Exception as e:
             print(e)
             await interaction.response.send_message("Something went wrong :S, pls tell my creator about it ^_^", ephemeral=True)
 
